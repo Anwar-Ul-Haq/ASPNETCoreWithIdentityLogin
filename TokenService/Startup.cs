6e605fec3a1c48e3dbb85bd04e214784d470a84d
Anwar Ul-Haq <aulhaq6@gmail.com>
2019-06-12 22:49:36 +0100

    added external login (google)

--- TokenService/Startup.cs
+++ TokenService/Startup.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer4;
 using IdentityServer4.Quickstart.UI;
 using IdentityServer4.Test;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Server.HttpSys;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace TokenService
@@ -17,11 +22,22 @@ namespace TokenService
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+           
             services.AddIdentityServer().AddDeveloperSigningCredential()
                 .AddInMemoryIdentityResources(Config.GetIdentityResources())
                 .AddInMemoryClients(Config.GetClients())
                 .AddTestUsers(TestUsers.Users);
-                services.AddMvc();
+
+            services.AddAuthentication().AddGoogle("Google" ,googleoptions =>
+            {
+                googleoptions.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                googleoptions.ClientId = "551467361169-c2k9et776jknfvpvfnnmr9eqhfsvjpro.apps.googleusercontent.com";
+                googleoptions.ClientSecret = "IGl_pe7fFLaunu4PqZp3gpjZ";
+
+            });
+
+            services.AddMvc();
+           
 
         }
 
@@ -34,7 +50,6 @@ namespace TokenService
             }
 
             app.UseIdentityServer();
-
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
         }
