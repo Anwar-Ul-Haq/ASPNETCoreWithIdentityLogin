45c06d8fc42ba4f6561d073eaf65fe68f0b533bb
Anwar Ul-Haq <aulhaq6@gmail.com>
2019-06-12 21:07:04 +0100

    added identity token service

---
+++ TokenService/Config.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using IdentityServer4;
+using IdentityServer4.Models;
+
+namespace TokenService
+{
+    public class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+                new IdentityResource
+                {
+                    Name = "Role",
+                    UserClaims = new List<string>
+                        {"Role" }
+                }
+            };
+        }
+
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "AnwarAcademyMVC",
+                    ClientName = "Anwar Academy MVC Client",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    RedirectUris = {"http://localhost:5002/signin-oidc"},
+                    PostLogoutRedirectUris = {"http://localhost:5002/ signout-callback-oidc"},
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email
+                    }
+                }
+            };
+        }
+
+
+
+    }
+    
+}
